8e3b156a3a816fa19489058e43e0bb150cdcb6a8
tranchinh <chinhlong30032001@gmail.com>
2023-08-26 17:32:34 +0700

    ok

--- src/Body.jsx
+++ src/Body.jsx
@@ -2,40 +2,44 @@ import React, { useEffect, useState } from 'react'
 import { Icon } from '@iconify/react';
 import { utils } from 'near-api-js';
 import { Wallet } from './near-wallet';
-import { UsdtContract } from './usdt-interface';
 function Body() {
     const [wallet, setWallet] = useState();
-    console.log("wallet: ", wallet)
-    const [contract2, setContract2] = useState();
+    const [accountId, setAccountId] = useState("");
+    const [isModal, setIsModal] = useState(false);
+    console.log("wallet: ", accountId)
     useEffect(() => {
         window.Buffer = window.Buffer || require("buffer").Buffer;
         const loadWallet = async() => {
-        const wallet = new Wallet({
-            createAccessKeyFor: "hkt2plats.testnet",
-            network: "testnet"
-        })
-        const contract2 = new UsdtContract({
-        contractId: "ft1.tranchinh2001.testnet",
-        walletToUse: wallet
-    })
-    setContract2(contract2)
-    setWallet(wallet);
-    await wallet.startUp();
-}
-
-   loadWallet();
-  }, [])
-  const handleUsdtSponse = async () => {
-    const newAmount = utils.format.parseNearAmount(10);
-    await contract2.sponseUsdt("00009", newAmount);
-  }
-  const handleClick = () => {
-    if(!wallet?.accountId) {
-        wallet.signIn()
+            const wallet = new Wallet({
+                createAccessKeyFor: "hkt2plats.testnet",
+                network: "testnet"
+            })
+            setWallet(wallet);
+            await wallet.startUp();
+        }
+        if(wallet?.accountId) {
+            setAccountId(wallet.accountId);
+        }
+        loadWallet();
+    }, [wallet?.accountId])
+  const handleClick = async() => {
+    if(!accountId){
+        setIsModal(true);
     }
     else {
-        handleUsdtSponse();
+        const amount = utils.format.parseNearAmount("100")
+        await wallet.callMethod({
+            contractId: "ft1.tranchinh2001.testnet",
+            method: "ft_transfer_call",
+            args: {receiver_id: "hkt2plats.testnet", amount, msg: "5621"},
+            deposit: 1,
+            gas: "200000000000000"
+        })
     }
+  }
+  const handleSignIn = async() => {
+     await wallet.signIn();
+    setIsModal(false);
   }
 
   return (
@@ -81,7 +85,7 @@ function Body() {
                         </div>
                     </div>
                     <div className='mt-[10px] rounded-[5px] border-[1px] border-[#D1D1D1] flex items-center justify-center gap-[4px]' >
-                        <button onClick={() => wallet.signOut()} className='pt-[5px] pb-[7px] px-[10px] rounded-[10px] my-[15px] text-[#fff] bg-[#008B8B]' >Connect Wallet</button>
+                        {/* <button onClick={() => wallet.signOut()} className='pt-[5px] pb-[7px] px-[10px] rounded-[10px] my-[15px] text-[#fff] bg-[#008B8B]' >Connect Wallet</button> */}
                         <button onClick={() => handleClick()} className='pt-[5px] pb-[7px] px-[10px] rounded-[10px] my-[15px] text-[#fff] bg-[#187fe2]'>Submit</button>
                     </div>
                     <h2 className='text-[20px] pt-[23px] text-left text-[#111343] leading-6' >Top events</h2>
@@ -89,9 +93,36 @@ function Body() {
             </div>
         </div>
     </div>
-    <div className='pt-[50px] pb-[40px] bg-[#2D2E32] mt-[800px]' >
+        <div className='pt-[50px] pb-[40px] bg-[#2D2E32] mt-[800px]' >
         </div>
+         {isModal && (
+                <div className="relative z-10" aria-labelledby="modal-title" role="dialog" aria-modal="true">
+                    <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
+                    <div className="fixed inset-0 z-10 overflow-y-auto">
+                        <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+                            <div className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+                                <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+                                    <div className="sm:flex sm:items-start">
+                                        <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
+                                        </div>
+                                        <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
+                                            <h3 className="text-base font-semibold leading-6 text-gray-900" id="modal-title">Notification</h3>
+                                            <div className="mt-2">
+                                                <p className="text-sm text-gray-500">You are not logged in near wallet.</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+                                    <button onClick={handleSignIn} type="button" className="mt-3 inline-flex w-full justify-center rounded-md bg-[#187fe2] px-3 py-2 text-sm font-semibold shadow-sm ring-1 ring-inset ring-gray-300 hover:opacity-60 sm:mt-0 sm:w-auto text-white">Connect Wallet</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            )}
     </div>
+    
   )
 }
 
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Header from './Header'
 import Body from './Body'
+import "./index.css"
 
 function Home() {
   return (
